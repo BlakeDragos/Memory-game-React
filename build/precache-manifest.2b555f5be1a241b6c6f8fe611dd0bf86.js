bb1390f6f452e1f40a334b9d16f0536feac94d06
Blake D <Mr.duncan8@gmail.com>
2019-04-27 16:46:58 -0500

    still texting

---
+++ build/precache-manifest.2b555f5be1a241b6c6f8fe611dd0bf86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1adb4b54866f718431bc9b97eb7ab46",
+    "url": "/BlakeDragos/Memory-game-React/index.html"
+  },
+  {
+    "revision": "83ebf85c5f95a5cf6015",
+    "url": "/BlakeDragos/Memory-game-React/static/css/main.d4aa8e2d.chunk.css"
+  },
+  {
+    "revision": "ed67e21e0fbb680b5f8f",
+    "url": "/BlakeDragos/Memory-game-React/static/js/2.abafc83a.chunk.js"
+  },
+  {
+    "revision": "83ebf85c5f95a5cf6015",
+    "url": "/BlakeDragos/Memory-game-React/static/js/main.51a9e609.chunk.js"
+  },
+  {
+    "revision": "2d7f977ee44a5c5d9f70",
+    "url": "/BlakeDragos/Memory-game-React/static/js/runtime~main.387da430.js"
+  }
+]);
