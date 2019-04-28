407f803708bba300d625748614bef756832a1da0
Blake D <Mr.duncan8@gmail.com>
2019-04-27 19:44:12 -0500

    Updates

--- precache-manifest.d5ad5add67ee0a6a7d11e63215f9106a.js
+++ precache-manifest.d5ad5add67ee0a6a7d11e63215f9106a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3da776e7c530b955458c6961529a7657",
+    "revision": "c7063159daf74ac325057ffd59a17d2e",
     "url": "/Memory-game-React/index.html"
   },
   {
-    "revision": "83ebf85c5f95a5cf6015",
+    "revision": "19d30a57931b819d16e7",
     "url": "/Memory-game-React/static/css/main.d4aa8e2d.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Memory-game-React/static/js/2.abafc83a.chunk.js"
   },
   {
-    "revision": "83ebf85c5f95a5cf6015",
-    "url": "/Memory-game-React/static/js/main.51a9e609.chunk.js"
+    "revision": "19d30a57931b819d16e7",
+    "url": "/Memory-game-React/static/js/main.4c183c93.chunk.js"
   },
   {
     "revision": "a92d8037c2bea8f1e6a1",
